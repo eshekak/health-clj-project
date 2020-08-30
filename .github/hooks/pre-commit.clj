1f2639b61ab6e5c35fe6e016e13b76239ebaf75f
eshekak <olenev.biz@gmail.com>
2020-08-30 21:33:53 +0300

    feat/ci-cd: Test pre-commit hook x7

--- .github/hooks/pre-commit.clj
+++ .github/hooks/pre-commit.clj
@@ -1,7 +1,9 @@
 (try (load-file ".github/common-scripts/validate-branch-name.clj")
      (load-file ".github/common-scripts/format-code.clj")
      (load-file ".github/common-scripts/lint-code.clj")
-     ; (load-file ".github/common-scripts/run-units.clj")
+     (load-file ".github/common-scripts/run-units.clj")
      ; (load-file ".github/common-scripts/kibit-lint.clj")
      (catch Exception _
-       (System/exit 1)))
+       (do
+         (println "Error in pre-commit hook!")
+         (System/exit 1))))
