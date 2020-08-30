8037c66960a41d174f863865c0c6641ecf6884a4
eshekak <olenev.biz@gmail.com>
2020-08-30 21:29:06 +0300

    feat/ci-cd: Test pre-commit hook x4

--- .github/hooks/pre-commit.clj
+++ .github/hooks/pre-commit.clj
@@ -1,6 +1,6 @@
 (try (load-file ".github/common-scripts/validate-branch-name.clj")
      (load-file ".github/common-scripts/format-code.clj")
-     ; (load-file ".github/common-scripts/lint-code.clj")
+     (load-file ".github/common-scripts/lint-code.clj")
      ; (load-file ".github/common-scripts/run-units.clj")
      ; (load-file ".github/common-scripts/kibit-lint.clj")
      (catch Exception _
